75e519e99b2a2380fb649043363a4d23e20398eb
AroaAyuso <aroa.ayuso@gmail.com>
2019-04-12 14:32:35 +0200

    Pantalla muerte e inicio animacion (no funciona)

--- practica3.js
+++ practica3.js
@@ -18,9 +18,41 @@ window.addEventListener("load",function() {
         stage.add("viewport").follow(player, {x: true});
         stage.viewport.offsetX = -200;
 
-        stage.insert(new Q.Goomba({x : 950, y : 535}));
-        stage.insert(new Q.Bloopa({x : 450, y : 100}));
+        stage.insert(new Q.Goomba({x : 700, y : 535}));
+        stage.insert(new Q.Goomba({x : 1200, y : 535}));
+        stage.insert(new Q.Goomba({x : 1300, y : 535}));
+
+        stage.insert(new Q.Bloopa({x : 850}));
+        stage.insert(new Q.Bloopa({x : 950, y: 450}));
+        stage.insert(new Q.Bloopa({x : 1050}));
+
     });
+
+    Q.scene('endGame',function(stage) {
+        var container = stage.insert(new Q.UI.Container({
+          x: Q.width/2, y: Q.height/2, fill: "rgba(0,0,0,0.5)"
+        }));
+        var button = container.insert(new Q.UI.Button({ x: 0, y: 0, fill: "#CCCCCC",label: "Play Again" }))         
+        var label = container.insert(new Q.UI.Text({x:0, y: -10 - button.p.h, label: stage.options.label }));
+        button.on("click",function() {
+            Q.clearStages();
+            Q.stageScene('level1');
+        });
+        container.fit(20);
+    });
+
+    Q.scene('mainTitle',function(stage) {
+        var container = stage.insert(new Q.UI.Container({
+          x: Q.width/2, y: Q.height/2, fill: "rgba(0,0,0,0.5)"
+        }));
+        var button = container.insert(new Q.UI.Button({ x: 0, y: 0, fill: "#CCCCCC",label: "Start Game" }))         
+        button.on("click",function() {
+            Q.clearStages();
+            Q.stageScene('level1');
+        });
+        container.fit(20);
+    });
+
 
     Q.Sprite.extend("Mario", {
         init: function(p) {
@@ -31,7 +63,13 @@ window.addEventListener("load",function() {
                 speed: 300,
             });
             this.add('2d, platformerControls');
-            this.on("hit.sprite",function(collision){});
+            this.on("hit.sprite",function(collision){
+                /*if(collision.obj.isA("Princess")) {
+                    Q.stageScene("endGame",1, { label: "You Won!" }); 
+                    this.destroy();
+                  }
+                */
+            });
         },
 
         step: function(dt) {
@@ -53,12 +91,14 @@ window.addEventListener("load",function() {
         init: function(p) {
             this._super(p, {
                 sheet: "goomba",
+                vx: 0.1
                 //gravity: 0.5
             });
             this.add("2d, aiBounce");
 
             this.on("bump.left, bump.right, bump.bottom", function(collision) {
                 if(collision.obj.isA("Mario")) {
+                    Q.stageScene("endGame",1, { label: "You Died" }); 
                     collision.obj.destroy();
                 }
             });
@@ -73,18 +113,17 @@ window.addEventListener("load",function() {
     Q.Sprite.extend("Bloopa", {
         init: function(p) {
             this._super(p, {
-                //sprite: "bloopa",
+                sprites: "bloopa",
                 sheet: "bloopa",
-                //visibleOnly: true,
-                gravity: 0.3,
-                visibleOnly: true,
-                vy: 0.15
+                gravity: 0.4,
+                vy: 0.1,
+                y: 400
             });
-            this.add("2d");
+            this.add("2d, animations");
             
             this.on("bump.left,bump.right,bump.bottom",function(collision) {
                 if(collision.obj.isA("Mario")) { 
-                 // Q.stageScene("endGame",1, { label: "You Died" }); 
+                  Q.stageScene("endGame",1, { label: "You Died" }); 
                   collision.obj.destroy();
                 }
               });
@@ -94,18 +133,33 @@ window.addEventListener("load",function() {
                   this.destroy();
                 }
             });
+
+            this.on("bump.bottom",this, "jump");
+            this.play("alive");
            
-        }
+        },
+        jump: function(dt){
+            this.p.vy=-300;
+        },
+        
+            
+        
     });
-	
+   
 
     Q.load(["mario_small.png", "mario_small.json",
             "goomba.png", "goomba.json",
             "bloopa.png", "bloopa.json"
+           // "princess.png","princess.json"
         ], function() {
         Q.compileSheets("mario_small.png", "mario_small.json");
         Q.compileSheets("goomba.png", "goomba.json");
         Q.compileSheets("bloopa.png", "bloopa.json");
+       // Q.compileSheets("princess.png","princess.json");
+       Q.animations("bloopa", {
+        alive:{frames: [0,1], rate: 1/6, loop: true},
+        dead:{frames: [1], rate: 1/4, loop: false}
+        });
     });
 
 });
