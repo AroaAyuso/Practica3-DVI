83a65ecca33f2a0c258f62526ac91d682725e279
JaSanz <javiersanz13@gmail.com>
2019-04-12 11:16:53 +0200

    Goomba. No sé si nada funciona

--- practica3.js
+++ practica3.js
@@ -14,9 +14,11 @@ window.addEventListener("load",function() {
 
     Q.scene("level1", function(stage) {
         Q.stageTMX("level.tmx", stage);
-        var player = stage.insert(new Q.Mario());
+        var player = stage.insert(new Q.Mario({x : 210, y : 535}));
         stage.add("viewport").follow(player, {x: true});
         stage.viewport.offsetX = -200;
+
+        stage.insert(new Q.Goomba({x : 400, y : 400}));
     });
 
     Q.Sprite.extend("Mario", {
@@ -26,8 +28,6 @@ window.addEventListener("load",function() {
                 sheet: "marioR",
                 jumpSpeed: -525,
                 speed: 300,
-                x : 210,
-                y : 535
             });
             this.add('2d, platformerControls');
             this.on("hit.sprite",function(collision){});
@@ -48,8 +48,32 @@ window.addEventListener("load",function() {
         
     });
 
-    Q.load(["mario_small.png", "mario_small.json"], function() {
-        Q.compileSheets("mario_small.png", "mario_small.json");
+    Q.Sprite.extend("Goomba", {
+        init: function(p) {
+            this._super(p, {
+                sheet: "goomba",
+                gravity: 0.5
+            });
+            this.add("2d");
+
+            this.on("bump.left, bump.right, bump.bottom", function(colision) {
+                if(colision.obj.isA("Player")) {
+                    collision.obj.destroy();
+                }
+            });
+            /*this.on("bump.top", function(colision) {
+                if(colision.obj.isA("Player")) {
+                    this.destroy();
+                }
+            });*/
+        }
+    });
+
+    Q.load(["mario_small.png", "mario_small.json",
+            "goomba.png", "goomba.json"
+        ], function() {
+        Q.compileSheets("mario_small.png", "mario_small.json",
+            "goomba.png", "goomba.json");
     });
 
 });
